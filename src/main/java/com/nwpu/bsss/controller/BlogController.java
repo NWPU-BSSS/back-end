73d41f340654d229a6f5372264b6ee00bec29d50
Zeege <5666377+zeege@user.noreply.gitee.com>
2020-11-22 19:15:15 +0800

    [fix] readd GET /blog/comments in BlogController

--- src/main/java/com/nwpu/bsss/controller/BlogController.java
+++ src/main/java/com/nwpu/bsss/controller/BlogController.java
@@ -1,11 +1,10 @@
 package com.nwpu.bsss.controller;
 
 import com.nwpu.bsss.domain.BlogEntity;
-import com.nwpu.bsss.domain.UserEntity;
 import com.nwpu.bsss.domain.dto.PostBlogBody;
-import com.nwpu.bsss.domain.dto.ReleaseBlogBody;
-import com.nwpu.bsss.repository.BlogRepository;
-import com.nwpu.bsss.response.*;
+import com.nwpu.bsss.response.Code;
+import com.nwpu.bsss.response.GetBlogResponse;
+import com.nwpu.bsss.response.MyResponseEntity;
 import com.nwpu.bsss.response.blog.CommentElement;
 import com.nwpu.bsss.service.BlogService;
 import com.nwpu.bsss.service.CommentService;
@@ -18,49 +17,56 @@ import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
 
-@RestController("/blog")
+@RestController
 public class BlogController {
-
-    @Resource
-    private BlogService blogService;
-    @Resource
-    private UserService userService;
-    @Resource
-    private CommentService commentService;
-    @Resource
-    private LikeService likeService;
-
-
-    @GetMapping("/blog")
-    public MyResponseEntity<GetBlogResponse> getBlogInfo(@RequestParam("blogId") int blogId) {
-
-        BlogEntity blogEntity = this.blogService.findByBlogId(blogId);
-        if (blogEntity == null)
-            return new MyResponseEntity<>(Code.BAD_OPERATION,"没有找到博客",null);
-
-        GetBlogResponse getBlogResponse = new GetBlogResponse();
-        getBlogResponse.setTitle(blogEntity.getTitle());
-        getBlogResponse.setContent(blogEntity.getContent());
-        getBlogResponse.setLikesNum(this.likeService.getLikesNum(blogId));
-        getBlogResponse.setCommentsNum(this.commentService.getCommentsNum(blogId));
-        getBlogResponse.setShareNum(77L);//to-do
-        getBlogResponse.setFavoriteNum(88L);
-
-        return new MyResponseEntity<>(Code.OK,"ok",getBlogResponse);
-    }
-
-    @PostMapping("/blog")
-    public MyResponseEntity postBlog(@RequestHeader("accessToken") String accessToken,
-                                     @RequestBody PostBlogBody body) {
-        Long userId = UserController.token2Id.get(accessToken);
-        if(userId==null)
-          return new MyResponseEntity(Code.BAD_OPERATION,"token无效！！",null);
-
-        BlogEntity blogEntity = new BlogEntity(body.getTitle(), body.getTagA(), body.getTagB(),body.getTagC(),userId,
-                new Timestamp(new Date().getTime()),new Timestamp(new Date().getTime()),body.getContent());
-
-        blogService.saveBlog(blogEntity);
-        return new MyResponseEntity(Code.OK,"博客发布成功",null);
-
-    }
+	
+	@Resource
+	private BlogService blogService;
+	@Resource
+	private UserService userService;
+	@Resource
+	private CommentService commentService;
+	@Resource
+	private LikeService likeService;
+	
+	@GetMapping("/blog/comments")
+	public MyResponseEntity<List<CommentElement>> getComments(@RequestParam("blogId") long blogId) {
+		List<CommentElement> commentList = this.commentService.getCommentList(blogId);
+		return new MyResponseEntity<>(Code.OK, "ok", commentList);
+	}
+	
+	@GetMapping("/blog")
+	public MyResponseEntity<GetBlogResponse> getBlogInfo(@RequestParam("blogId") int blogId) {
+		
+		BlogEntity blogEntity = this.blogService.findByBlogId(blogId);
+		if (blogEntity == null) {
+			return new MyResponseEntity<>(Code.BAD_OPERATION, "没有找到博客", null);
+		}
+		
+		GetBlogResponse getBlogResponse = new GetBlogResponse();
+		getBlogResponse.setTitle(blogEntity.getTitle());
+		getBlogResponse.setContent(blogEntity.getContent());
+		getBlogResponse.setLikesNum(this.likeService.getLikesNum(blogId));
+		getBlogResponse.setCommentsNum(this.commentService.getCommentsNum(blogId));
+		getBlogResponse.setShareNum(77L);//to-do
+		getBlogResponse.setFavoriteNum(88L);
+		
+		return new MyResponseEntity<>(Code.OK, "ok", getBlogResponse);
+	}
+	
+	@PostMapping("/blog")
+	public MyResponseEntity postBlog(@RequestHeader("accessToken") String accessToken,
+	                                 @RequestBody PostBlogBody body) {
+		Long userId = UserController.token2Id.get(accessToken);
+		if (userId == null) {
+			return new MyResponseEntity(Code.BAD_OPERATION, "token无效！！", null);
+		}
+		
+		BlogEntity blogEntity = new BlogEntity(body.getTitle(), body.getTagA(), body.getTagB(), body.getTagC(), userId,
+				new Timestamp(new Date().getTime()), new Timestamp(new Date().getTime()), body.getContent());
+		
+		this.blogService.saveBlog(blogEntity);
+		return new MyResponseEntity(Code.OK, "博客发布成功", null);
+		
+	}
 }
