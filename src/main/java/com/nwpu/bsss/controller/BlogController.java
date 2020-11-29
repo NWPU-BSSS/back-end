d723d6976482936630072e99a84934244d5337d3
Zeege <5666377+zeege@user.noreply.gitee.com>
2020-11-29 17:29:01 +0800

    [fix] bug in GET /blog

--- src/main/java/com/nwpu/bsss/controller/BlogController.java
+++ src/main/java/com/nwpu/bsss/controller/BlogController.java
@@ -4,7 +4,6 @@ import com.nwpu.bsss.domain.BlogEntity;
 import com.nwpu.bsss.domain.CommentEntity;
 import com.nwpu.bsss.domain.dto.PostBlogBody;
 import com.nwpu.bsss.domain.dto.PostCommentBody;
-import com.nwpu.bsss.domain.dto.PostBlogBody;
 import com.nwpu.bsss.response.Code;
 import com.nwpu.bsss.response.GetBlogResponse;
 import com.nwpu.bsss.response.MyResponseEntity;
@@ -46,8 +45,8 @@ public class BlogController {
 		GetBlogResponse getBlogResponse = new GetBlogResponse();
 		getBlogResponse.setTitle(blogEntity.getTitle());
 		getBlogResponse.setContent(blogEntity.getContent());
-		getBlogResponse.setLikesNum(this.likeService.getLikesNum(blogId));
-		getBlogResponse.setCommentsNum(this.commentService.getCommentsNum(blogId));
+		getBlogResponse.setLikeNum(this.likeService.getLikesNum(blogId));
+		getBlogResponse.setCommentNum(this.commentService.getCommentsNum(blogId));
 		getBlogResponse.setShareNum(77L);//to-do
 		getBlogResponse.setFavoriteNum(88L);
 		
@@ -56,7 +55,7 @@ public class BlogController {
 	
 	@PostMapping("/blog")
 	public MyResponseEntity<Object> postBlog(@RequestHeader("accessToken") String accessToken,
-	                                 @RequestBody PostBlogBody body) {
+	                                         @RequestBody PostBlogBody body) {
 		Long userId = UserController.token2Id.get(accessToken);
 		if (userId == null) {
 			return new MyResponseEntity<>(Code.BAD_OPERATION, "token无效", null);
@@ -69,20 +68,20 @@ public class BlogController {
 		return new MyResponseEntity<>(Code.OK, "博客发布成功", null);
 		
 	}
-
+	
 	@PostMapping("/blog/comment")
-	public MyResponseEntity postComment(@RequestHeader("accessToken") String accessToken,
-									 @RequestParam("blogId") long blogId,
-									 @RequestBody PostCommentBody body) {
+	public MyResponseEntity<Object> postComment(@RequestHeader("accessToken") String accessToken,
+	                                            @RequestParam("blogId") long blogId,
+	                                            @RequestBody PostCommentBody body) {
 		Long userId = UserController.token2Id.get(accessToken);
 		if (userId == null) {
-			return new MyResponseEntity(Code.BAD_OPERATION, "token无效", null);
+			return new MyResponseEntity<>(Code.BAD_OPERATION, "token无效", null);
 		}
-
-		CommentEntity commentEntity=new CommentEntity(userId,blogId,body.getCommentId(),body.getContent());
-
+		
+		CommentEntity commentEntity = new CommentEntity(userId, blogId, body.getCommentId(), body.getContent());
+		
 		this.commentService.saveComment(commentEntity);
-		return new MyResponseEntity(Code.OK, "博客发布成功", null);
-
+		return new MyResponseEntity<>(Code.OK, "博客发布成功", null);
+		
 	}
 }
